c24114dadf59cf7bf6d7835b42bf4f7ea74d389a
linrichh <linapu82@gmail.com>
2025-08-28 19:57:17 +0200

    Calculator Update
    looped calculations, throw error when divideByZero, soft exit out of program, invalid operator error

--- python_exercises/Calculator.py
+++ python_exercises/Calculator.py
@@ -2,42 +2,58 @@
 # 2- mehrfachberechnungen ohne programm neu zu starten, fehlermeldungen bei falschem datentyp/div durch 0
 # 3- wurzel und expontialfkt, mehrere operationen und zahlen, menü mit optionen statt freie eingabe
 # 4- gui mit tkinter, parser für ausdrücke -> punkt vor strich
+import sys
 
 # operations
 addition = ["+", "plus"]
 subtraction = ["-", "minus"]
 multiplication = ["*", "x", "times", "mal"]
 division = ["/", "durch", "divided by"]
-modulo = ["%", "mod"]
+modulo = ["%", "mod", "modulo"]
 
 # inputs
-input_whole = input(print("What should i calculate?: "))
-input_parts = input_whole.split()
-num_1 = input_parts[0]
-op_1 = input_parts[1]
-num_2 = input_parts[2]
+status = True
+while status:
+    input_whole = input("Type \"exit\" to stop calculator. What should i calculate?: ")
+    if input_whole == "exit":
+        status = False
+        print("Calculator closed.")
+        break
+    else:
+        input_parts = input_whole.split()
+        num_1 = input_parts[0]
+        op_1 = input_parts[1]
+        num_2 = input_parts[2]
 
-op = "no input"
-result = "no result"
+    op = "no input"
+    result = "no result"
 
-if op_1 in addition:
-    op = " + "
-    result = str(float(num_1) + float(num_2))
+    if op_1 in addition:
+        op = " + "
+        result = str(float(num_1) + float(num_2))
 
-if op_1 in subtraction:
-    op = " - "
-    result = str(float(num_1) - float(num_2))
+    elif op_1 in subtraction:
+        op = " - "
+        result = str(float(num_1) - float(num_2))
 
-if op_1 in multiplication:
-    op = " * "
-    result = str(float(num_1) * float(num_2))
+    elif op_1 in multiplication:
+        op = " * "
+        result = str(float(num_1) * float(num_2))
 
-if op_1 in division:
-    op = " / "
-    result = str(float(num_1) / float(num_2))
+    elif op_1 in division:
+        try:
+            op = " / "
+            result = str(float(num_1) / float(num_2))
+        except ZeroDivisionError:
+            print("Division through Zero not possible.")
+            continue
 
-if op_1 in modulo:
-    op = " % "
-    result = str(float(num_1) % float(num_2))
+    elif op_1 in modulo:
+        op = " mod "
+        result = str(float(num_1) % float(num_2))
 
-print("Result: " + num_1 + op + num_2 + " = " + result)
+    else:
+        print("Invalid operator. Calculation not possible.")
+        continue
+
+    print("Result: " + num_1 + op + num_2 + " = " + result)
--- python_exercises/Test1.py
+++ python_exercises/Test1.py
@@ -2,3 +2,11 @@ print("""venv erstellen hat grad alles gekostet nh, aber glaub ich habs.
 War mies anstrengend aber iwie.
 Muss mal was essen gehen jetzt :) 
 GaLiGrü""")
+
+numbersToText = {
+    1: "one",
+    2: "two",
+    3: "three"
+}
+print(numbersToText.get(2))
+
