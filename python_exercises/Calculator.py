2eba247d19ecb4e6a21be5ad49af172ed058f836
linrichh <linapu82@gmail.com>
2025-08-28 18:27:33 +0200

    basic basic calculator first steps

---
+++ python_exercises/Calculator.py
@@ -0,0 +1,43 @@
+# 1- eine operation, 2 zahlen, input vom user, programm gibt ergebnis
+# 2- mehrfachberechnungen ohne programm neu zu starten, fehlermeldungen bei falschem datentyp/div durch 0
+# 3- wurzel und expontialfkt, mehrere operationen und zahlen, menü mit optionen statt freie eingabe
+# 4- gui mit tkinter, parser für ausdrücke -> punkt vor strich
+
+# operations
+addition = ["+", "plus"]
+subtraction = ["-", "minus"]
+multiplication = ["*", "x", "times", "mal"]
+division = ["/", "durch", "divided by"]
+modulo = ["%", "mod"]
+
+# inputs
+input_whole = input(print("What should i calculate?: "))
+input_parts = input_whole.split()
+num_1 = input_parts[0]  # iwie erste zahl rausnehmen
+op_1 = input_parts[1]  # ersten operator iwie
+num_2 = input_parts[2]  # zweite zahl usw
+
+op = "no input"
+result = "no result"
+
+if op_1 in addition:
+    op = " + "
+    result = str(float(num_1) + float(num_2))
+
+if op_1 in subtraction:
+    op = " - "
+    result = str(float(num_1) - float(num_2))
+
+if op_1 in multiplication:
+    op = " * "
+    result = str(float(num_1) * float(num_2))
+
+if op_1 in division:
+    op = " / "
+    result = str(float(num_1) / float(num_2))
+
+if op_1 in modulo:
+    op = " % "
+    result = str(float(num_1) % float(num_2))
+
+print("Ergebnis: " + num_1 + op + num_2 + " = " + result)
